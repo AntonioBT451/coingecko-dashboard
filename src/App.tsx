862b0165223d28a0086a8c34dcd26ac9b4c9ce0d
AntonioBT451 <antoniobt451@gmail.com>
2026-03-07 18:22:17 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Box, Container, Typography } from '@mui/material';
+
+export const App = () => {
+  return (
+    <Container maxWidth='lg' className=''>
+      <Box sx={{ my: 4 }} >
+        <Typography variant='h4' component='h1' gutterBottom className='font-bold text-cyan-950' >
+          Dashboard Dinametra
+        </Typography>
+      </Box>
+    </Container>
+  )
+};
---
+++ src/test/App.test.ts
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'vitest';
+
+describe('App', () => {
+
+    test('should ', () => {
+
+        const cadena = 'Esto es una cadena'
+        expect(cadena).toBe('Esto es una cadena');
+    })
+
+})
